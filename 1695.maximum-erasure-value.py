088fe14e98863d6482e46d9d845642882942d5bd
RS <rshikhhamzayev@crimson.ua.edu>
2026-02-17 08:49:50 -0600

    Problem 1695

---
+++ 1695.maximum-erasure-value.py
@@ -0,0 +1,80 @@
+#
+# @lc app=leetcode id=1695 lang=python
+#
+# [1695] Maximum Erasure Value
+#
+# https://leetcode.com/problems/maximum-erasure-value/description/
+#
+# algorithms
+# Medium (64.12%)
+# Likes:    3391
+# Dislikes: 66
+# Total Accepted:    291.9K
+# Total Submissions: 455.2K
+# Testcase Example:  '[4,2,4,5,6]'
+#
+# You are given an array of positive integers nums and want to erase a subarray
+# containing unique elements. The score you get by erasing the subarray is
+# equal to the sum of its elements.
+# 
+# Return the maximum score you can get by erasing exactly one subarray.
+# 
+# An array b is called to be a subarray of a if it forms a contiguous
+# subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some
+# (l,r).
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [4,2,4,5,6]
+# Output: 17
+# Explanation: The optimal subarray here is [2,4,5,6].
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [5,2,1,2,5,2,1,2,5]
+# Output: 8
+# Explanation: The optimal subarray here is [5,2,1] or [1,2,5].
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 10^5
+# 1 <= nums[i] <= 10^4
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def maximumUniqueSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        start = 0
+        max_sum = 0
+        sum = 0
+        seen = set()
+        for i, num in enumerate(nums):
+            if num not in seen:
+                seen.add(num)
+                sum += num
+            else:
+                while nums[start] != num:
+                    sum -= nums[start]
+                    seen.remove(nums[start])
+                    start += 1
+                start += 1
+
+            if sum > max_sum:
+                max_sum = sum
+        
+        return max_sum
+# @lc code=end
+
